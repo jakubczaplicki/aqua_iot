c40b8f92f540b65a294e4b4223f3d423acf7303f
Jakub Czaplicki <jakub.czaplicki@tooploox.com>
2020-05-03 13:05:59 +0200

    Add counters

--- aqua_temp.ino
+++ aqua_temp.ino
@@ -1,9 +1,4 @@
-// This #include statement was automatically added by the Particle IDE.
-#include <OneWire.h>
-
-// This #include statement was automatically added by the Particle IDE.
 #include <Adafruit_SSD1306.h>
-
 #include <MQTT.h>
 #include "DS18.h"
 
@@ -21,16 +16,20 @@ DS18 tempSensor(D0);
 double temperature;
 bool publish_temperature = false;
 bool send_mqqt = false;
-// screen
-int  x, minX; // variables for scrolling code
 
 // mqtt
 void mqtt_callback(char* topic, byte* payload, unsigned int length);
 void timer_callback_temperature_check();
 void timer_callback_send_mqqt_data();
 
-Timer temperatureTimer(5 * 60 * 1000, timer_callback_temperature_check);
-Timer mqttTimer(15 * 60 * 1000, timer_callback_send_mqqt_data);
+const int tempTimerCountDownConst = 5 * 60;
+const int mqttTimerCountDownConst = 15 * 60;
+// screen
+int tPub = tempTimerCountDownConst;
+int tMqtt = mqttTimerCountDownConst;
+Timer counterTimer(1000, timer_callback_count_seconds);
+Timer temperatureTimer(tempTimerCountDownConst * 1000, timer_callback_temperature_check);
+Timer mqttTimer(mqttTimerCountDownConst * 1000, timer_callback_send_mqqt_data);
 
 byte server[] = { 192, 168, 0, 108 };
 MQTT client(server, 1883, MQTT_KEEPALIVE, mqtt_callback);
@@ -41,6 +40,11 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
     memcpy(p, payload, length);
     p[length] = NULL;
     Particle.publish("mqtt", p, 3600, PRIVATE);
+}
+
+void timer_callback_count_seconds() {
+    tPub--;
+    tMqtt--;
 }
 
 void timer_callback_temperature_check() {
@@ -85,7 +89,8 @@ void setup() {
   }
   LEDStatus(RGB_COLOR_WHITE, LED_PATTERN_BLINK, LED_SPEED_FAST, LED_PRIORITY_NORMAL);
 
-  // start timers 
+  // start timers
+  counterTimer.start();
   temperatureTimer.start();
   mqttTimer.start();
 }
@@ -93,24 +98,41 @@ void setup() {
 void loop() {
     if (tempSensor.read())
         temperature = tempSensor.celsius();
-
     display.clearDisplay();
+    display.setTextSize(2);
     display.setCursor(0, 5);
     display.print(temperature);
+    display.print(" C");
     display.display();
 
     if (publish_temperature)
     {
+      tPub = tempTimerCountDownConst;
+      display.setTextSize(1);
+      display.setCursor(90, 5);
+      display.print("PUB");
+      display.display();
       Particle.publish("temp", String(temperature, 1), 3600, PRIVATE);
       publish_temperature = false;
     }
+    else
+    {
+      display.setTextSize(1);
+      display.setCursor(90, 5);
+      display.print(tPub);
+      display.display();
+    }
 
-    
     if (send_mqqt) 
     {
+      tMqtt = mqttTimerCountDownConst;
       digitalWrite(D7, HIGH);
       if (client.isConnected()) 
       {
+        display.setTextSize(1);
+        display.setCursor(90, 15);
+        display.print("MQTT");
+        display.display();
         Particle.publish("mqtt", "Pushing " + String(temperature, 1) + " to Home Assistant", 3600, PRIVATE);
         client.publish("photon/aqua/temp", String(temperature, 1));
       }
@@ -123,12 +145,22 @@ void loop() {
         } 
         else 
         {
+          display.setTextSize(1);
+          display.setCursor(90, 15);
+          display.print("ERROR");
+          display.display();
           Particle.publish("mqtt", "Failed to re-connect to Home Assistant", 3600, PRIVATE);
         }
       }
-      delay(1000);
       digitalWrite(D7, LOW);
       send_mqqt = false;
+    }
+    else 
+    {
+      display.setTextSize(1);
+      display.setCursor(90, 15);
+      display.print(tMqtt);
+      display.display();
     }
 }
 
